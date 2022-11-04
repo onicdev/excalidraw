29e6c3085eba423e2ab51b6a3b2d332bbae9dd9d
Vitaliy Grusha <vit.grusha@gmail.com>
2022-11-04 13:48:34 +0200

    Update export

--- src/packages/excalidraw/index.tsx
+++ src/packages/excalidraw/index.tsx
@@ -237,5 +237,10 @@ export {
 export * from "../../constants";
 export { jotaiStore } from "../../jotai";
 export { ErrorDialog } from "../../components/ErrorDialog";
-export * as CollabReconciliation from "../../excalidraw-app/collab/reconciliation";
-export * as Utils from "../../utils";
+export { AbortError } from "../../errors";
+export * from "../../excalidraw-app/collab/reconciliation";
+export * from "../../utils";
+export * from "../../element/typeChecks";
+export * from "../../element/mutateElement";
+export * from "../../element/mutateElement";
+export { t as translate } from "../../i18n";
