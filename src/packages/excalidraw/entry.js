3298adea32139eed24680938243625adf67eb601
Vitaliy Grusha <vit.grusha@gmail.com>
2022-11-04 16:41:35 +0200

    Update export

--- src/packages/excalidraw/entry.js
+++ src/packages/excalidraw/entry.js
@@ -5,13 +5,16 @@ import "../../../public/fonts.css";
 
 polyfill();
 export * from "./index";
-export * from "../../constants";
-export { jotaiStore } from "../../jotai";
-export { ErrorDialog } from "../../components/ErrorDialog";
+
+// Custom for Neembboard
 export { AbortError } from "../../errors";
+export { ErrorDialog } from "../../components/ErrorDialog";
+export { jotaiStore } from "../../jotai";
+export { t as translate } from "../../i18n";
+export * from "../../constants";
+export * from "../../data/encryption";
+export * from "../../element/mutateElement";
+export * from "../../element/typeChecks";
 export * from "../../excalidraw-app/collab/reconciliation";
 export * from "../../utils";
-export * from "../../element/typeChecks";
-export * from "../../element/mutateElement";
-export * from "../../element/mutateElement";
-export { t as translate } from "../../i18n";
+export * from "../../analytics";
--- src/packages/excalidraw/index.tsx
+++ src/packages/excalidraw/index.tsx
@@ -232,15 +232,3 @@ export {
   sceneCoordsToViewportCoords,
   viewportCoordsToSceneCoords,
 } from "../../utils";
-
-// Custom for Neembboard
-export * from "../../constants";
-export { jotaiStore } from "../../jotai";
-export { ErrorDialog } from "../../components/ErrorDialog";
-export { AbortError } from "../../errors";
-export * from "../../excalidraw-app/collab/reconciliation";
-export * from "../../utils";
-export * from "../../element/typeChecks";
-export * from "../../element/mutateElement";
-export * from "../../element/mutateElement";
-export { t as translate } from "../../i18n";
