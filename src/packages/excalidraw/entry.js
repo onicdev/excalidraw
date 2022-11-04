6c3526391e93c7cb17371c9d6d5cb34a6b31419a
Vitaliy Grusha <vit.grusha@gmail.com>
2022-11-04 14:18:24 +0200

    Add exports to entry

--- src/packages/excalidraw/entry.js
+++ src/packages/excalidraw/entry.js
@@ -5,3 +5,13 @@ import "../../../public/fonts.css";
 
 polyfill();
 export * from "./index";
+export * from "../../constants";
+export { jotaiStore } from "../../jotai";
+export { ErrorDialog } from "../../components/ErrorDialog";
+export { AbortError } from "../../errors";
+export * from "../../excalidraw-app/collab/reconciliation";
+export * from "../../utils";
+export * from "../../element/typeChecks";
+export * from "../../element/mutateElement";
+export * from "../../element/mutateElement";
+export { t as translate } from "../../i18n";
