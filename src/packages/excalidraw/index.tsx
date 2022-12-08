760175820b25b27be3a1d32071c6aead388c45a1
a-yerzhyk <andriiyerzh@gmail.com>
2022-12-08 09:56:47 +0200

    Update exports

--- src/packages/excalidraw/index.tsx
+++ src/packages/excalidraw/index.tsx
@@ -236,3 +236,18 @@ export {
 } from "../../utils";
 
 export { Sidebar } from "../../components/Sidebar/Sidebar";
+
+// Custom for Neembboard
+export { AbortError } from "../../errors";
+export { ErrorDialog } from "../../components/ErrorDialog";
+export { jotaiStore } from "../../jotai";
+export { t as translate } from "../../i18n";
+export * from "../../constants";
+export * from "../../data/encryption";
+export * from "../../element/mutateElement";
+export * from "../../element/typeChecks";
+export * from "../../excalidraw-app/collab/reconciliation";
+export * from "../../utils";
+export * from "../../analytics";
+export * from "../../data/encode";
+export * from "../../data/restore";
