8e749d595c1b49a4c82de53b1d3aedb08a5ad961
a-yerzhyk <andriiyerzh@gmail.com>
2022-12-08 09:57:12 +0200

    Dont show left menu when importing image

--- src/element/showSelectedShapeActions.ts
+++ src/element/showSelectedShapeActions.ts
@@ -1,16 +1,28 @@
 import { AppState } from "../types";
 import { NonDeletedExcalidrawElement } from "./types";
 import { getSelectedElements } from "../scene";
-
 export const showSelectedShapeActions = (
   appState: AppState,
   elements: readonly NonDeletedExcalidrawElement[],
 ) =>
   Boolean(
     (!appState.viewModeEnabled &&
+      appState.activeTool.type !== "image" &&
       appState.activeTool.type !== "custom" &&
       (appState.editingElement ||
         (appState.activeTool.type !== "selection" &&
           appState.activeTool.type !== "eraser"))) ||
-      getSelectedElements(elements, appState).length,
+      (getSelectedElements(elements, appState).length &&
+        !isOnlyImageSelected(appState, elements)),
   );
+
+// Dont show shape actions for image
+const isOnlyImageSelected = (
+  appState: AppState,
+  elements: readonly NonDeletedExcalidrawElement[],
+) => {
+  const selectedElements = elements.filter(
+    (el) => appState.selectedElementIds[el.id],
+  );
+  return selectedElements.every((el) => el.type === "image");
+};
